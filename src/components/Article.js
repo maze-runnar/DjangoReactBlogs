ab19763b742e3aee2dde9e794ed018c38ede7ea6
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2020-05-22 11:10:53 +0530

    dark mode implementation

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { List, Avatar, Icon } from "antd";
 
+
+const lightmode = {
+  background: 'white',
+  color: 'black',
+};
+const darkmode = {
+  background:'#273746',
+  color:'white'
+}
+
 const IconText = ({ type, text }) => (
   <span>
     <Icon
@@ -29,9 +39,10 @@ const Articles = props => {
         <List.Item
           key={item.title}
           actions={[
-            <IconText type="star-o" text="156" />,
-            <IconText type="like-o" text="156" />,
-            <IconText type="message" text="2" />
+            <Icon type="star-o" style ={props.darkMode? darkmode : lightmode} />,
+            <Icon type="like-o" text="156" style={props.darkMode? darkmode : lightmode} />,
+            <Icon type="message" style = {props.darkMode? darkmode : lightmode} />,
+            <Icon type="link" style = {props.darkMode? darkmode : lightmode} />
           ]}
           extra={
             <img
@@ -41,13 +52,14 @@ const Articles = props => {
             />
           }
 
-
+          style = {props.darkMode? darkmode : lightmode}
 
         >
           <List.Item.Meta
-            avatar={<Avatar src={item.avatar} />}
-            title={<a href={`/articles/${item.id}`}> {item.title} </a>}
+            avatar={<Avatar src="https://www.desktopbackground.org/p/2013/07/15/607734_35-harry-potter-wallpapers-35-210-harry-potter-hd-wallpapers_1920x1080_h.jpg" />}
+            title={<a href={`/articles/${item.id}`}> <span style ={props.darkMode? darkmode : lightmode} >{item.title} </span></a>}
             description={item.description}
+
           />
           {item.content}
         </List.Item>
